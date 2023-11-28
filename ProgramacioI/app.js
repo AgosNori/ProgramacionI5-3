59508bd3b76a0ecf3e4fa66ca905a463798bcaba
Agos Noriega <69058577+AgosNori@users.noreply.github.com>
2023-11-28 13:41:11 -0300

    Subiendo los trabajos

---
+++ ProgramacioI/app.js
@@ -0,0 +1,49 @@
+// Clase 1
+// este es un comentario de una linea 
+/* comentario 
+multilinea
+*/
+ 
+// Las variables son una caja que almacenan un valor
+var miNombre;
+
+// Almacenar un valor en la variable creada
+miNombre = "Agos";
+
+// Ejercicio 1:
+// Crear una variable edad y guardarle un numero
+
+var edad; // creamos la variable
+edad = 22;  // la inicializamos 
+console.log(edad); // esto muestra en la consola 
+
+// Ejercicio 2:
+// Inicializar tres variables a, b y c con los valores 
+// 5, 10 y "I am a" 
+
+// // Cambia solo el código debajo de esta línea
+var a = 5;
+var b = 10;
+var c = "I am a";
+// // Cambia solo el código encima de esta línea
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+console.log(a);
+console.log(b);
+console.log(c);
+
+// Clase 2
+var nombre = prompt('Ingrese su nombre: ')
+console.log('Hola',nombre); 
+
+// Clase 3
+// Ejercicio 3
+// Calcular el area y el perimetro de un cuadrado
+let lado = 40;
+let area = lado * lado;
+let perimetro = lado * 4;
+console.log('El area de un cuadrado es: ',area);
+console.log('EL perimetro de un cuadrado es: ',perimetro);
+
